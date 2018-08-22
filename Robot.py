51b5ac28d7826f8bd5ea1c20f3445610dfaccd13
chord <zhoushengyuan@jiuletech.com>
2018-08-22 10:20:13 +0800

    write Robot.py

--- Robot.py
+++ Robot.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Robot(object):
 
     def __init__(self, maze, alpha=0.5, gamma=0.9, epsilon0=0.5):
@@ -42,9 +43,11 @@ class Robot(object):
         """
         if self.testing:
             # TODO 1. No random choice when testing
+            self.epsilon = 0
             pass
         else:
             # TODO 2. Update parameters when learning
+            self.epsilon -= 0.003
             pass
 
         return self.epsilon
@@ -55,7 +58,7 @@ class Robot(object):
         """
 
         # TODO 3. Return robot's current state
-        return None
+        return self.maze.sense_robot()
 
     def create_Qtable_line(self, state):
         """
@@ -66,39 +69,46 @@ class Robot(object):
         # Qtable[state] ={'u':xx, 'd':xx, ...}
         # If Qtable[state] already exits, then do
         # not change it.
-        pass
+        val = {val: 0.0 for val in self.valid_actions}
+        self.Qtable.setdefault(state, val)
 
     def choose_action(self):
         """
         Return an action according to given rules
         """
+
         def is_random_exploration():
 
             # TODO 5. Return whether do random choice
             # hint: generate a random number, and compare
             # it with epsilon
-            pass
+            rand = random.random()
+            return rand < self.epsilon
 
         if self.learning:
             if is_random_exploration():
                 # TODO 6. Return random choose aciton
-                return None
+                return random.choice(self.valid_actions)
             else:
                 # TODO 7. Return action with highest q value
-                return None
+                return max(self.Qtable[self.state], key=self.Qtable[self.state].get)
         elif self.testing:
             # TODO 7. choose action with highest q value
+            return max(self.Qtable[self.state], key=self.Qtable[self.state].get)
         else:
             # TODO 6. Return random choose aciton
+            return random.choice(self.valid_actions)
 
     def update_Qtable(self, r, action, next_state):
         """
         Update the qtable according to the given rule.
         """
         if self.learning:
-            pass
             # TODO 8. When learning, update the q table according
             # to the given rules
+            max_reward = max(self.Qtable[next_state].values())
+            self.Qtable[self.state][action] = (1 - self.alpha) * self.Qtable[self.state][action] + self.alpha * (
+                        r + self.gamma * max_reward)
 
     def update(self):
         """
@@ -106,17 +116,17 @@ class Robot(object):
         Called every time in every epoch in training or testing.
         Return current action and reward.
         """
-        self.state = self.sense_state() # Get the current state
-        self.create_Qtable_line(self.state) # For the state, create q table line
+        self.state = self.sense_state()  # Get the current state
+        self.create_Qtable_line(self.state)  # For the state, create q table line
 
-        action = self.choose_action() # choose action for this state
-        reward = self.maze.move_robot(action) # move robot for given action
+        action = self.choose_action()  # choose action for this state
+        reward = self.maze.move_robot(action)  # move robot for given action
 
-        next_state = self.sense_state() # get next state
-        self.create_Qtable_line(next_state) # create q table line for next state
+        next_state = self.sense_state()  # get next state
+        self.create_Qtable_line(next_state)  # create q table line for next state
 
         if self.learning and not self.testing:
-            self.update_Qtable(reward, action, next_state) # update q table
-            self.update_parameter() # update parameters
+            self.update_Qtable(reward, action, next_state)  # update q table
+            self.update_parameter()  # update parameters
 
         return action, reward
